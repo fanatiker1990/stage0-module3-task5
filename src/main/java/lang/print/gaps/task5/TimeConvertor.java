e3fd62b3f78c84863c1338bc700d0e5368f18ba0
fanatiker1990 <59375292+fanatiker1990@users.noreply.github.com>
2023-05-04 15:34:10 +0300

    1

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        System.out.println(minutes*60);
+    }
+
+    public static void main(String[] args) {
+        TimeConvertor t=new TimeConvertor();
+        t.convert(11.5F);
     }
 }
